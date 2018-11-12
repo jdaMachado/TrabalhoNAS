6164b5faf7525093415ddfcfb6124f263e97eefc
jdaMachado <34648293+jdaMachado@users.noreply.github.com>
2018-11-12 18:43:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin:'bottom'
+});
+
+sr.reveal('.showcase-left', {
+    duration: 4000,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('.showcase-right', {
+    duration: 2000,
+    origin:'right',
+    distance:'300px'
+});
+sr.reveal('.showcase-btn', {
+    duration: 2000,
+    delay: 2000,
+    origin:'bottom'
+});
+sr.reveal('#objective div', {
+    duration: 2000,
+    origin:'bottom'
+});
+sr.reveal('.info-left', {
+    duration: 2000,
+    origin:'left',
+    distance:'300px',
+    viewFactor: 0.2
+});
+sr.reveal('.info-right', {
+    duration: 2000,
+    origin:'right',
+    distance:'300px',
+    viewFactor: 0.2
+});
+
+sr.reveal('div h3', {
+    duration: 7000,
+});
+
+sr.reveal('.gama', {
+    duration: 2000,
+});
+
+//Item images
+sr.reveal('#item1', {
+    duration: 2000,
+    origin:'top',
+    distance:'70px',
+});
+sr.reveal('#btn1', {
+    duration: 2000,
+});
+
+
+sr.reveal('#item2', {
+    duration: 2500,
+    origin:'top',
+    distance:'70px', 
+});
+sr.reveal('#btn2', {
+    duration: 2500,
+});
+
+
